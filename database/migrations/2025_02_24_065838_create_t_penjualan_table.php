26e5a4c1a0164fa85ef54416204cd21ed569437f
Jessica Amelia <ameliajessica997@gmail.com>
2025-02-24 14:10:53 +0700

    prak 2.2

---
+++ database/migrations/2025_02_24_065838_create_t_penjualan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index(); // indexing untuk ForeignKey
+            $table->string('pembeli', 20); 
+            $table->string('penjualan_kode', 20);
+            $table->string('penjualan_tanggal', );
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};

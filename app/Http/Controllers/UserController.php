4089349609e02bd74543363f02bedd3f95b6b892
Jessica Amelia <ameliajessica997@gmail.com>
2025-03-04 20:27:02 +0700

    Prak2.3_JS4_Minggu4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
-
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        //dd($user);
         return view('user', ['data' => $user]);
     }
 }

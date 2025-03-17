1897c8ce54c905493c745e96cf817cbd7cbe7453
Jessica Amelia <ameliajessica997@gmail.com>
2025-03-18 00:04:14 +0700

    A.Template Form (AdminLTE)_JS6_Minggu6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,87 @@
-@extends('layouts.app')
-@extends('adminlte::page').
-{{-- Customize layout sections --}}
-@section('title', 'Welcome')
-@section('header_title', 'Home')
-@section('header_subtitle', 'Welcome')
-
-{{-- Content body: main page content --}}
-@section('content_body')
-    <p>Welcome to this beautiful admin panel.</p>
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+<h1>Dashboard</h1>
 @stop
 
-{{-- Push extra CSS --}}
-@push('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@endpush
+@section('content')
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <!-- Form untuk m_user -->
+      <div class="card card-primary">
+        <div class="card-header">
+          <h3 class="card-title">Form untuk tabel m_user</h3>
+        </div>
+        <form id="quickForm">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username" placeholder="Masukkan username">
+            </div>
+            <div class="form-group">
+              <label for="nama">Nama</label>
+              <input type="text" class="form-control" id="nama" placeholder="Masukkan nama">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" placeholder="Masukkan password">
+            </div>
+            <div class="form-group">
+              <label for="level">Level</label>
+              <select class="form-control" id="level">
+                <option>Administrator</option>
+                <option>Manager</option>
+                <option>Staff</option>
+              </select>
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="termsCheck">
+              <label class="form-check-label" for="termsCheck">
+                I agree to the <a href="#" class="text-primary font-weight-bold">terms of service</a>.
+              </label>
+            </div>
+            
+          </div>
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
 
-{{-- Push extra scripts --}}
-@push('js')
-    {{-- <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script> --}}
-@endpush
+    <div class="col-md-6">
+      <!-- Form untuk m_level -->
+      <div class="card card-info">
+        <div class="card-header">
+          <h3 class="card-title">Form untuk tabel m_level</h3>
+        </div>
+        <form>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="levelName">Nama Level</label>
+              <input type="text" class="form-control" id="levelName" placeholder="Masukkan nama level">
+            </div>
+            <div class="form-group">
+              <label for="levelCode">Kode Level</label>
+              <input type="text" class="form-control" id="levelCode" placeholder="Masukkan kode level">
+            </div>
+          </div>
+          <div class="card-footer">
+            <button type="submit" class="btn btn-info">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
+
+@section('css')
+{{-- Add here extra stylesheets --}}
+{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+<script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop

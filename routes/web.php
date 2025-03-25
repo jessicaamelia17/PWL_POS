bb324e407b0b0790aa5d3a6affcabf5c32d57a44
Jessica Amelia <ameliajessica997@gmail.com>
2025-03-25 10:54:27 +0700

    PRAK3_JS5_REVISI

--- routes/web.php
+++ routes/web.php
@@ -2,45 +2,20 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UserController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-// Perbaikan prak 2.6 
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])
-     ->name('kategori.edit');
-Route::put('/kategori/{id}', [KategoriController::class, 'update'])
-     ->name('kategori.update');
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])
-     ->name('kategori.destroy');
-Route::get('/level/tambah', [LevelController::class, 'create']);
-
-Route::post('/level/store', [LevelController::class, 'store'])->name('level.store');
-
-Route::resource('m_user', POSController::class);
-
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);       // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);    // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);// menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);      // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);    // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);// menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);  // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);// menghapus data user
+});

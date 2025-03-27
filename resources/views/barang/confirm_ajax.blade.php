5c54d3f186ecd0cca43da36c05408d8f2b887a7a
Jessica Amelia <ameliajessica997@gmail.com>
2025-03-27 22:39:24 +0700

    Perbaikan TUGAS m_barang

--- resources/views/barang/confirm_ajax.blade.php
+++ resources/views/barang/confirm_ajax.blade.php
@@ -38,7 +38,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
-                <button type="submit" class="btn btn-danger">Ya, Hapus</button>
+                <button type="submit" class="btn btn-primary">Ya, Hapus</button>
             </div>
         </div>
     </div>

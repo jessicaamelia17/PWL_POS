56316221df160c399e3c9408123af3c12c1d9f9c
Jessica Amelia <ameliajessica997@gmail.com>
2025-04-14 14:38:20 +0700

    Park4 Tugas 4 JS7

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserModel;
+use App\Models\LevelModel;
 
 class AuthController extends Controller
 {
@@ -46,6 +48,45 @@ class AuthController extends Controller
 
         return redirect('login');
     }
+    public function register(){
+         $level = LevelModel::select('level_id','level_nama')->get();
+ 
+         return View('auth.register')
+         ->with('level', $level);
+     }
+ 
+     public function store_user(Request $request)
+     {
+         $request->validate([
+             'username' => 'required|string|min:3|unique:m_user,username',
+             'nama'     => 'required|string|max:100',
+             'password' => 'required|min:5',
+             'level_id' => 'required|integer',
+         ]);
+     
+         // Menyimpan user baru
+         UserModel::create([
+             'username' => $request->username,
+             'nama'     => $request->nama,
+             'password' => bcrypt($request->password), // Enkripsi password
+             'level_id' => $request->level_id,
+         ]);
+     
+         // Mengirim respons JSON jika permintaan AJAX
+         if ($request->ajax() || $request->wantsJson()) {
+             return response()->json([
+                 'status' => true,
+                 'message' => 'Registrasi Berhasil',
+                 'redirect' => url('login')
+             ]);
+         }
+     
+         // Jika bukan AJAX, redirect ke halaman utama dengan flash message
+         return redirect('/')->with('success', 'Registrasi berhasil');
+     }
+     
+ 
+
 }
 
 

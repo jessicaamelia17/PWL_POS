94b066b5c17ba0da18cd716ecb9920165a4c9fe0
Jessica Amelia <ameliajessica997@gmail.com>
2025-05-03 22:21:21 +0700

    TUGAS CRUD API m_user JS10

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,11 +20,19 @@ use App\Http\Controllers\Api\LevelController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+Route::get('users', [UserController::class, 'index']);
+ Route::post('users', [UserController::class, 'store']);
+ Route::get('users/{user}', [UserController::class, 'show']);
+ Route::put('users/{user}', [UserController::class, 'update']);
+ Route::delete('users/{user}', [UserController::class, 'destroy']);
+
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
